d668a66de6562c81abb726fa25e4c7761c530d85
ivankp <ivan.pogrebnyak@gmail.com>
2023-02-22 14:04:14 -0500

    convert_mxaods mc

--- src/convert_mxaods.cc
+++ src/convert_mxaods.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <filesystem>
 #include <optional>
+#include <regex>
 #include <cstdlib>
 #include <cerrno>
 
@@ -28,7 +29,7 @@ using ivan::cat, ivan::branch_reader;
 constexpr float fnan = std::numeric_limits<float>::quiet_NaN();
 
 std::filesystem::path dir;
-bool is_mc = false;
+bool mc = false;
 uint64_t nevents = 0;
 float lumi = 0, weight = 0;
 double mc_factor = 0;
@@ -64,7 +65,7 @@ class output_file {
 
 public:
   output_file(std::string_view name)
-  : path(dir/cat(name, is_mc ? "_mc" : "_data", ".dat")),
+  : path(dir/cat(name, mc ? "_mc" : "_data", ".dat")),
     fd([this]{
       int fd = ::open(path.c_str(), O_CREAT|O_WRONLY|O_TRUNC, 0644);
       if (fd < 0) ERROR('\"',path.native(),"\": open(): ",strerror(errno));
@@ -73,9 +74,9 @@ public:
     buf( static_cast<char*>( ::malloc(buflen) ) ),
     m(buf)
   {
-    static constexpr size_t skip = sizeof(lumi) + sizeof(nevents);
-    const size_t nulls = 8-((name.size()+2+skip)%8);
-    const size_t bufneed = name.size()+nulls+2+skip;
+    const size_t skip = 2ul + (!mc ? sizeof(lumi) : 0ul) + sizeof(nevents);
+    const size_t nulls = 8-((name.size()+skip)%8);
+    const size_t bufneed = name.size()+nulls+skip;
     if (buflen < bufneed)
       ERROR('\"',path.native(),"\": buffer length ",buflen," , need ",bufneed);
 
@@ -85,16 +86,16 @@ public:
     memcpy(m,name.data(),name.size()); m += name.size();
     char tmp[8] { };
     memcpy(m,tmp,nulls); m += nulls;
-    *m++ = is_mc ? 'm' : 'd';
+    m[0] = mc ? 'm' : 'd';
     static_assert(type_byte<T> != '\0');
-    *m++ = type_byte<T>;
+    m[1] = type_byte<T>;
 
-    skipped = m - buf;
+    skipped = (m+2) - buf;
     m += skip;
   }
   ~output_file() {
     flush();
-    m = bufcpy(buf,lumi);
+    if (!mc) m = bufcpy(m,lumi);
     m = bufcpy(m,nevents);
     if ((
       ::lseek(fd,skipped,SEEK_SET)
@@ -133,22 +134,38 @@ public:
   }
 };
 
-int main(int argc, char** argv) {
-  if (argc < 4) {
-    cout << "usage: " << argv[0] << " lumi output_dir input.root ...\n";
+int main(int argc, char** argv) { try {
+  if (argc < 3) {
+    cout << "usage: " << argv[0] << " output_dir input.root ...\n";
     return 1;
   }
-  lumi = atof(argv[1]);
-  is_mc = lumi == 0;
 
   cout << "Input files:\n";
-  for (int i=3; i<argc; ++i) {
+  for (int i=2; i<argc; ++i) {
     cout << argv[i] << '\n';
+
+    // Luminosity
+    static const std::regex re(R"((\d+(?:\.\d*)?)ipb\b)");
+    std::cmatch match;
+    if (std::regex_search(argv[i],match,re)) {
+      if (i>2 &&  mc) ERROR("data file after mc files");
+      mc = false;
+      lumi += ivan::stox<double>(match[1].str());
+    } else {
+      if (i>2 && !mc) ERROR("mc file after data files");
+      mc = true;
+    }
   }
   cout << endl;
 
+  if (mc) {
+    cout << "Monte Carlo\n\n";
+  } else {
+    cout << "Data\nLumi = " << lumi << " ipb\n\n";
+  }
+
   // Open output files ==============================================
-  std::filesystem::create_directories(dir = argv[2]);
+  std::filesystem::create_directories(dir = argv[1]);
   cout << "Output files:\n";
 
   output_file<uint8_t> f_N_j_30("N_j_30");
@@ -193,24 +210,22 @@ int main(int argc, char** argv) {
 
   cout << endl;
 
-  for (int i=3; i<argc; ++i) { // loop over input files
+  for (int i=2; i<argc; ++i) { // loop over input files
     cout << argv[i] << endl;
     TFile file(argv[i]);
 
-    if (is_mc) { // MC
+    if (mc) {
       for (auto* key : *file.GetListOfKeys()) {
         const char* name = key->GetName();
         if (!ivan::starts_with(name,"CutFlow_") ||
             !ivan::ends_with(name,"_noDalitz_weighted")) continue;
-        TH1 *h = static_cast<TH1*>(static_cast<TKey*>(key)->ReadObj());
+        TH1 *h = dynamic_cast<TH1*>(static_cast<TKey*>(key)->ReadObj());
         cout << name << endl;
         const double n_all = h->GetBinContent(3);
         cout << h->GetXaxis()->GetBinLabel(3) << " = " << n_all << endl;
         mc_factor = 1e3/n_all;
         break;
       }
-    } else { // Data
-      // lumi += get_lumi(file);
     }
 
     TTreeReader reader("CollectionTree",&file);
@@ -224,7 +239,7 @@ int main(int argc, char** argv) {
     std::optional<branch_reader<Float_t>> b_cs_br_fe;
     std::optional<branch_reader<Float_t>> b_weight;
 
-    if (is_mc) {
+    if (mc) {
       b_isFiducial.emplace(reader, VAR_PREF_TRUTH "isFiducial");
       b_cs_br_fe.emplace(reader, VAR_PREF "crossSectionBRfilterEff");
       b_weight.emplace(reader, VAR_PREF "weight");
@@ -233,7 +248,7 @@ int main(int argc, char** argv) {
 #define VAR(TYPE,NAME) \
   branch_reader<TYPE> b_##NAME(reader, VAR_PREF #NAME); \
   std::optional<branch_reader<TYPE>> b_##NAME##_truth; \
-  if (is_mc) b_##NAME##_truth.emplace(reader, VAR_PREF_TRUTH #NAME);
+  if (mc) b_##NAME##_truth.emplace(reader, VAR_PREF_TRUTH #NAME);
 
     VAR(Int_t,N_j_30)
 
@@ -257,47 +272,46 @@ int main(int argc, char** argv) {
 
       ++nevents;
 
-      if (is_mc) {
-        weight = (**b_weight) * (**b_cs_br_fe) * mc_factor;
-      }
-
       f_m_yy.write(m_yy);
+
+      if (mc) {
+        weight = double(**b_weight) * double(**b_cs_br_fe) * mc_factor;
+        f_m_yy.write(weight);
+      }
 
       // ============================================================
       const uint8_t N_j_30 = ceiling_cast<uint8_t>(*b_N_j_30);
       f_N_j_30.write(N_j_30);
+      if (mc) f_N_j_30.write(ceiling_cast<uint8_t>(**b_N_j_30_truth));
 
 #define VAR(TYPE,NAME) \
   const TYPE NAME = *b_##NAME; \
-  f_##NAME.write(NAME);
+  f_##NAME.write(NAME); \
+  if (mc) f_##NAME.write<TYPE>(**b_##NAME##_truth);
 
 #define VAR_1e3(NAME) \
   const float NAME = *b_##NAME * 1e-3; \
-  f_##NAME.write(NAME);
+  f_##NAME.write(NAME); \
+  if (mc) f_##NAME.write<float>(**b_##NAME##_truth * 1e-3);
 
-#define VARS \
-  VAR(Float_t,abs_Zepp) \
-  VAR( Char_t,catXS_lepton) \
-  VAR( Char_t,catXS_MET) \
-  VAR( Char_t,catXS_nbjet) \
-  VAR( Char_t,catXS_ttH) \
-  VAR( Char_t,catXS_VBF) \
-  VAR(Float_t,HT_30) \
-  VAR(Float_t,yAbs_yy) \
-  \
-  VAR_1e3(pT_yy) \
-  VAR_1e3(pT_yy_JV_30) \
-  VAR_1e3(pT_yy_JV_40) \
-  VAR_1e3(pT_yy_JV_50) \
-  VAR_1e3(pT_yy_JV_60) \
-  VAR_1e3(rel_DpT_y_y) \
-  VAR_1e3(rel_pT_y1) \
-  VAR_1e3(rel_pT_y2) \
-  VAR_1e3(rel_sumpT_y_y)
+      VAR(Float_t,abs_Zepp)
+      VAR( Char_t,catXS_lepton)
+      VAR( Char_t,catXS_MET)
+      VAR( Char_t,catXS_nbjet)
+      VAR( Char_t,catXS_ttH)
+      VAR( Char_t,catXS_VBF)
+      VAR(Float_t,HT_30)
+      VAR(Float_t,yAbs_yy)
 
-      VARS
-
-#undef VARS
+      VAR_1e3(pT_yy)
+      VAR_1e3(pT_yy_JV_30)
+      VAR_1e3(pT_yy_JV_40)
+      VAR_1e3(pT_yy_JV_50)
+      VAR_1e3(pT_yy_JV_60)
+      VAR_1e3(rel_DpT_y_y)
+      VAR_1e3(rel_pT_y1)
+      VAR_1e3(rel_pT_y2)
+      VAR_1e3(rel_sumpT_y_y)
 
 #undef VAR
 #undef VAR_1e3
@@ -307,37 +321,33 @@ int main(int argc, char** argv) {
 
 #define VAR(NAME) \
   const float NAME = enough_jets ? float(*b_##NAME) : fnan; \
-  f_##NAME.write(NAME);
+  f_##NAME.write(NAME); \
+  if (mc) f_##NAME.write(enough_jets ? float(**b_##NAME##_truth) : fnan);
 
 #define VAR_1e3(NAME) \
   const float NAME = enough_jets ? float(*b_##NAME * 1e-3) : fnan; \
-  f_##NAME.write(NAME);
+  f_##NAME.write(NAME); \
+  if (mc) f_##NAME.write(enough_jets ? float(**b_##NAME##_truth * 1e-3) : fnan);
 
-#define VARS \
-  VAR(pT_j1_30) \
-  VAR(pT_yyj_30) \
-  VAR(m_yyj_30) \
-  VAR(maxTau_yyj_30) \
-  VAR(sumTau_yyj_30)
-
-      VARS
-
-#undef VARS
+      VAR(pT_j1_30)
+      VAR(pT_yyj_30)
+      VAR(m_yyj_30)
+      VAR(maxTau_yyj_30)
+      VAR(sumTau_yyj_30)
 
       // 2 jets =====================================================
       enough_jets = N_j_30 >= 2;
 
-#define VARS \
-  VAR(Dphi_j_j_30) \
-  VAR(Dphi_j_j_30_signed) \
-  VAR(Dphi_yy_jj_30) \
-  \
-  VAR_1e3(pT_yyjj_30) \
-  VAR_1e3(m_jj_30)
+      VAR(Dphi_j_j_30)
+      VAR(Dphi_j_j_30_signed)
+      VAR(Dphi_yy_jj_30)
 
-      VARS
+      VAR_1e3(pT_yyjj_30)
+      VAR_1e3(m_jj_30)
 
-#undef VARS
-    }
-  }
-}
+    } // event loop
+  } // file loop
+} catch (const std::exception& e) {
+  cerr << "\033[0;31m" << e.what() << "\033[0m\n";
+  return 1;
+}} // main
